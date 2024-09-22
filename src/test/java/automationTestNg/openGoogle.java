a05af6fa10bd252f3cb9f68762b384ae7ddd14b3
mpavanka <pavankalyan68335@gmail.com>
2024-09-22 17:59:23 +0530

    added assertion

--- src/test/java/automationTestNg/openGoogle.java
+++ src/test/java/automationTestNg/openGoogle.java
@@ -1,9 +1,7 @@
 package automationTestNg;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.*;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -12,15 +10,21 @@ public class openGoogle {
 
     static Page page;
     static Browser browser;
-    static BrowserType browserType;
+    static BrowserContext context;
     static Playwright playwright;
 
     @BeforeClass
     public static void beforeStart() {
+        // Initialize Playwright and launch the browser
         playwright = Playwright.create();
-        browserType = playwright.chromium();
         browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-        page = browser.newPage();
+
+        // Create a browser context (use the same context throughout)
+        Browser.NewContextOptions contextOptions = new Browser.NewContextOptions().setViewportSize(null);
+        context = browser.newContext(contextOptions);
+
+        // Create a new page inside the context
+        page = context.newPage();
     }
 
     @AfterClass
@@ -29,9 +33,9 @@ public class openGoogle {
         browser.close();
     }
 
-
     @Test
     public void searchForGoogleCeo() {
+        // Navigate to Google and search for "GOOGLE CEO"
         page.navigate("https://www.google.com");
         page.fill("[name='q']", "GOOGLE CEO");
         page.keyboard().press("Enter");
@@ -39,4 +43,12 @@ public class openGoogle {
         System.out.println(name);
     }
 
+    @Test(dependsOnMethods="searchForGoogleCeo")
+    public void clickOnInstaLink() throws Throwable {
+        page.evaluate("window.scrollBy(0, 1000)");
+        page.waitForSelector("//*[@id=\"rso\"]/div[3]/div/div/div[1]/div/div/span/a/h3", new Page.WaitForSelectorOptions().setTimeout(5000));
+        page.locator("[class=\"uq_cd\"]").click();
+        String errorMessage = page.locator("[class=\"eu_h\"]").textContent();
+        Assert.assertEquals(errorMessage, "D22", "We got permission to see google CEO profile in linkedIN");
+    }
 }
